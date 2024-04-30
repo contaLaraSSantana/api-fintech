325fd6862e2b6579f5e4af271a7ec9e85ea50a58
Lara S.Santana <124930864+contaLaraSSantana@users.noreply.github.com>
2024-04-30 15:19:36 -0300

    Update models.py

--- models.py
+++ models.py
@@ -6,7 +6,6 @@ class Usuario(db.Model):
     email = db.Column(db.String(100), unique=True)
     senha = db.Column(db.String(255))
 
-
 class Receitas(db.Model):
     id_receitas = db.Column(db.Integer, primary_key=True, autoincrement=True)
     descricao = db.Column(db.String(100))
@@ -14,14 +13,12 @@ class Receitas(db.Model):
     datas = db.Column(db.Date)
     id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'))
 
-
 class Despesas(db.Model):
     id_despesas = db.Column(db.Integer, primary_key=True, autoincrement=True)
     descricao = db.Column(db.String(100))
     valor = db.Column(db.Float(10.2))
     datas = db.Column(db.Date)
     id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'))
-
 
 class Poupanca(db.Model):
     id_poupanca = db.Column(db.Integer, primary_key=True, autoincrement=True)
