23f87b4f367222936e52b78838831a8c15cac349
Lara S.Santana <124930864+contaLaraSSantana@users.noreply.github.com>
2024-04-30 15:22:23 -0300

    Update models.py

--- models.py
+++ models.py
@@ -9,19 +9,19 @@ class Usuario(db.Model):
 class Receitas(db.Model):
     id_receitas = db.Column(db.Integer, primary_key=True, autoincrement=True)
     descricao = db.Column(db.String(100))
-    valor = db.Column(db.Float(10.2))
+    valor = db.Column(db.Float(10,2))
     datas = db.Column(db.Date)
-    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'))
+    id_usuario = db.Column(db.Integer, db.ForeignKey('Usuario.id_usuario'))
 
 class Despesas(db.Model):
     id_despesas = db.Column(db.Integer, primary_key=True, autoincrement=True)
     descricao = db.Column(db.String(100))
-    valor = db.Column(db.Float(10.2))
+    valor = db.Column(db.Float(10,2))
     datas = db.Column(db.Date)
-    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'))
+    id_usuario = db.Column(db.Integer, db.ForeignKey('Usuario.id_usuario'))
 
 class Poupanca(db.Model):
     id_poupanca = db.Column(db.Integer, primary_key=True, autoincrement=True)
     descricao = db.Column(db.String(100))
-    valor = db.Column(db.Float(10.2))
-    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'))
+    valor = db.Column(db.Float(10,2))
+    id_usuario = db.Column(db.Integer, db.ForeignKey('Usuario.id_usuario'))
